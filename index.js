ad667acf880e46dd655736973eec4f190c3750df
Richard Littauer <richard.littauer@gmail.com>
2017-10-03 08:13:18 -0400

    refactor: Use a global github function

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const path = require('path')
 const axios = require('axios')
 const env = require('./env')
 const console = require('./lib/robo')
+const github = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: {
+    common: {
+      authorization: `token ${env.BUILD_A_SPACE}`,
+      accept: [
+        `application/vnd.github.black-panther-preview+json`, // Community https://developer.github.com/v3/community
+        `application/vnd.github.scarlet-witch-preview+json` // CoC https://developer.github.com/v3/codes_of_conduct/
+      ]
+    }
+  }
+})
 
 // TODO Check that repoName is valid
 // TODO Export properly
@@ -15,41 +27,29 @@ async function buildASpace (repoName, diffs) {
   console.robolog(`Let's get some documentation up in here. Creating pull request ...`)
 
   // https://docs.travis-ci.com/user/environment-variables/
-  repoName = repoName || env.TRAVIS_REPO_SLUG
-  const github = axios.create({
-    baseURL: 'https://api.github.com',
-    headers: {
-      common: {
-        authorization: `token ${env.BUILD_A_SPACE}`,
-        accept: [
-          `application/vnd.github.black-panther-preview+json`, // Community https://developer.github.com/v3/community
-          `application/vnd.github.scarlet-witch-preview+json` // CoC https://developer.github.com/v3/codes_of_conduct/
-        ]
-      }
-    }
-  })
+  github.repoName = repoName || env.TRAVIS_REPO_SLUG
 
   // who am I?
   const {data: {login}} = await github.get('/user')
   console.robolog(`Signed in as ${login}. Looking if I already created a pull request.`)
+  github.login = login
+  github.branchName = await initPRandBranch()
 
-  const { branchName } = await initPRandBranch(github, login, repoName)
+  const communityFiles = await checkCommunityFiles()
 
-  const communityFiles = await checkCommunityFiles(github, repoName, branchName)
+  const files = await bunchFiles(communityFiles)
+  const jsFiles = await addJavascriptFiles()
 
-  const files = await bunchFiles(github, repoName, branchName, communityFiles)
-  const jsFiles = await addJavascriptFiles(github, repoName, branchName)
-
-  await createPullRequest(github, repoName, branchName, files.concat(jsFiles))
+  await createPullRequest(files.concat(jsFiles))
 }
 
-async function initPRandBranch (github, login, repoName) {
+async function initPRandBranch () {
   // Do I have a pending pull request?
   const query = querystring.stringify({
     type: 'pr',
-    author: login,
+    author: github.login,
     is: 'open',
-    repo: repoName
+    repo: github.repoName
   }, ' ', ':')
 
   const {data: pullRequestsResult} = await github.get(`/search/issues?q=${query}`)
@@ -59,7 +59,7 @@ async function initPRandBranch (github, login, repoName) {
   // I don’t know which one to update. So I’ll ask you for help :)
   if (pullRequestsResult.total_count > 1) {
     console.robolog('🤖🆘 Oh oh, I don’t know how to handle more than one pull requests. Creating an issue for my human friends')
-    const result = await github.post(`/repos/${repoName}/issues`, {
+    const result = await github.post(`/repos/${github.repoName}/issues`, {
       title: '🤖🆘 Too many PRs',
       body: `Dearest humans,
 
@@ -81,51 +81,48 @@ Hope you can fix it (and my circuits) soon 🙏`
     const pullRequest = pullRequestsResult.items[0]
     console.robolog(`Existing pull-request found: ${pullRequest.html_url}`)
 
-    const {data} = await github.get(`/repos/${repoName}/pulls/${pullRequest.number}`)
-    const branchName = data.head.ref
+    const {data} = await github.get(`/repos/${github.repoName}/pulls/${pullRequest.number}`)
+    github.branchName = data.head.ref // as branchName
   // TODO Enable existing pull request to be fixed and added to
-  // await updateFixtures({diffs, github, repoName, branchName})
+  // await updateFixtures({diffs, github, github.repoName, branchName})
   // console.robolog(`pull-request updated: ${pullRequest.html_url}`)
-    return {branchName}
   }
 
   console.robolog('No existing pull request found')
 
-  console.robolog(`Looking for last commit sha of ${repoName}/git/refs/heads/master`)
-  const {data: {object: {sha}}} = await github.get(`/repos/${repoName}/git/refs/heads/master`)
+  console.robolog(`Looking for last commit sha of ${github.repoName}/git/refs/heads/master`)
+  const {data: {object: {sha}}} = await github.get(`/repos/${github.repoName}/git/refs/heads/master`)
 
-  const branchName = `docs/boost-vitality/${new Date().toISOString().substr(0, 10)}`
+  github.branchName = `docs/boost-vitality/${new Date().toISOString().substr(0, 10)}`
 
   // Gets a 422 sometimes
-  const branchExists = await github.get(`/repos/${repoName}/branches/${branchName}`)
+  const branchExists = await github.get(`/repos/${github.repoName}/branches/${github.branchName}`)
     .catch(err => {
       if (err) {
-        console.robolog(`Creating new branch: ${branchName} using last sha ${sha}`)
+        console.robolog(`Creating new branch: ${github.branchName} using last sha ${sha}`)
       } // do nothing
     })
   if (!branchExists) {
-    await github.post(`/repos/${repoName}/git/refs`, {
-      ref: `refs/heads/${branchName}`,
+    await github.post(`/repos/${github.repoName}/git/refs`, {
+      ref: `refs/heads/${github.branchName}`,
       sha
     }).catch(err => {
       console.robofire('Unable to create a new branch. Do you have access?')
       return err
     })
   } else {
-    console.robolog(`Using existing branch: ${branchName} using last sha ${sha}`)
+    console.robolog(`Using existing branch: ${github.branchName} using last sha ${sha}`)
   }
-
-  return {branchName}
 }
 
 // TODO Lint the README
-// async function lintReadme (github, repoName, branchName) {
+// async function lintReadme () {
 //   console.robolog(`README linting not yet enabled`)
 // }
 
-async function addJavascriptFiles (github, repoName, branchName) {
+async function addJavascriptFiles () {
   // Is this a JS repo?
-  const {data: language} = await github.get(`/repos/${repoName}`)
+  const {data: language} = await github.get(`/repos/${github.repoName}`)
   if (!language === 'JavaScript') return
 
   console.robolog('Assuming this is a JavaScript repository, checking...')
@@ -133,7 +130,7 @@ async function addJavascriptFiles (github, repoName, branchName) {
   let toCheck = []
 
   // package.json checks
-  const {status, data: npm} = await github.get(`/repos/${repoName}/contents/package.json`).catch(err => err)
+  const {status, data: npm} = await github.get(`/repos/${github.repoName}/contents/package.json`).catch(err => err)
   if (status === 404) {
     console.robowarn('There is no package.json. Is this not checked into npm?')
     return
@@ -145,13 +142,13 @@ async function addJavascriptFiles (github, repoName, branchName) {
     // const query = querystring.stringify({
     //   type: 'issue',
     //   is: 'open',
-    //   repo: repoName
+    //   repo: github.repoName
     // }, 'tests', ':')
 
     // const testIssueResult = await github.get(`/search/issues?q=${query}`).catch(err => err)
     // console.log('Here', testIssueResult)
 
-    // const result = await github.post(`/repos/${repoName}/issues`, {
+    // const result = await github.post(`/repos/${github.repoName}/issues`, {
     //   title: 'Add tests',
     //   body: `No tests are specified in the npm manifest. Do you have tests for this repo yet?`
     // })
@@ -168,11 +165,11 @@ async function addJavascriptFiles (github, repoName, branchName) {
   }
 
   travisFile.content = await fs.readFileSync(path.join(__dirname, `fixtures/js/${travisFile.filePath}`)).toString('base64')
-  await github.put(`/repos/${repoName}/contents/${travisFile.filePath}?ref=${branchName}`, {
+  await github.put(`/repos/${github.repoName}/contents/${travisFile.filePath}?ref=${github.branchName}`, {
     path: travisFile.filePath,
     message: 'ci: adding travis file with Greenkeeper and semantic-release enabled',
     content: travisFile.content,
-    branch: branchName
+    branch: github.branchName
   }).catch(err => {
     if (err) {
       console.robowarn('Unable to add travis file!')
@@ -189,13 +186,12 @@ async function addJavascriptFiles (github, repoName, branchName) {
   // TODO Check that the license matches
   // TODO Check that the repository matches
 
-
   return toCheck
 }
 
-async function checkCommunityFiles (github, repoName, branchName) {
+async function checkCommunityFiles () {
   // what is the community vitality like?
-  const {data: community} = await github.get(`/repos/${repoName}/community/profile`)
+  const {data: community} = await github.get(`/repos/${github.repoName}/community/profile`)
     .catch(err => {
       console.robofire('Unable to get community profile. Check your headers.')
       return err
@@ -231,13 +227,13 @@ async function checkCommunityFiles (github, repoName, branchName) {
   async function existsInBranch (file) {
     if (!community.files[file.name]) {
       // Check if file exists already in the branch
-      const {status} = await github.get(`/repos/${repoName}/contents/${file.filePath}?ref=${branchName}`)
+      const {status} = await github.get(`/repos/${github.repoName}/contents/${file.filePath}?ref=${github.branchName}`)
       .catch(err => err)
       if (status !== 200) {
         let fileContent
 
         if (file.name === 'readme') {
-          fileContent = Buffer.from(`# ${repoName.split('/')[1]}
+          fileContent = Buffer.from(`# ${github.repoName.split('/')[1]}
 
 TODO This needs to be filled out!`).toString('base64')
           console.robowarn('You need to fill out the README manually!')
@@ -258,22 +254,22 @@ TODO This needs to be filled out!`).toString('base64')
 
 // This function bunches up multiple file changes into the same commit.
 // It depends on a files object: { name, filepath, content, note }.
-async function bunchFiles (github, repoName, branchName, filesToCheck) {
+async function bunchFiles (filesToCheck) {
   // Always work off of master, for now. TODO Enable other dev branches
   // get the current commit object and current tree
   const {
     data: {commit: {sha: currentCommitSha}},
     data: {commit: {commit: {tree: {sha: treeSha}}}}
-  } = await github.get(`/repos/${repoName}/branches/master`)
+  } = await github.get(`/repos/${github.repoName}/branches/master`)
 
   // retrieve the content of the blob object that tree has for that particular file path
-  const {data: {tree}} = await github.get(`/repos/${repoName}/git/trees/${treeSha}`)
+  const {data: {tree}} = await github.get(`/repos/${github.repoName}/git/trees/${treeSha}`)
 
   async function getFileBlob (file) {
     console.robolog(`Adding ${file.name} file`)
 
     // Create a blob
-    const {data: blob} = await github.post(`/repos/${repoName}/git/blobs`, {
+    const {data: blob} = await github.post(`/repos/${github.repoName}/git/blobs`, {
       content: file.content,
       encoding: 'base64'
     }).catch(err => {
@@ -298,20 +294,20 @@ async function bunchFiles (github, repoName, branchName, filesToCheck) {
     const newTree = tree.concat(newBlobs)
 
     // post a new tree object with that file path pointer replaced with your new blob SHA getting a tree SHA back
-    const {data: {sha: newTreeSha}} = await github.post(`/repos/${repoName}/git/trees`, {
+    const {data: {sha: newTreeSha}} = await github.post(`/repos/${github.repoName}/git/trees`, {
       tree: newTree,
       base_tree: treeSha
     })
 
     // create a new commit object with the current commit SHA as the parent and the new tree SHA, getting a commit SHA back
-    const {data: {sha: newCommitSha}} = await github.post(`/repos/${repoName}/git/commits`, {
+    const {data: {sha: newCommitSha}} = await github.post(`/repos/${github.repoName}/git/commits`, {
       message: `docs: adding community docs`,
       tree: newTreeSha,
       parents: [currentCommitSha]
     })
 
     // update the reference of your branch to point to the new commit SHA
-    await github.post(`/repos/${repoName}/git/refs/heads/${branchName}`, {
+    await github.post(`/repos/${github.repoName}/git/refs/heads/${github.branchName}`, {
       sha: newCommitSha
     }).catch(err => {
       if (err) {
@@ -322,8 +318,8 @@ async function bunchFiles (github, repoName, branchName, filesToCheck) {
   return filesToCheck
 }
 
-async function createPullRequest (github, repoName, branchName, files) {
-  if (branchName === 'master') {
+async function createPullRequest (files) {
+  if (github.branchName === 'master') {
     console.robolog(`No changes (you've run this already), or there is some other issue.`)
     console.log()
     return
@@ -338,9 +334,9 @@ Here are some things you should do manually before merging this Pull Request:
 ${files.map(file => `- [ ] ${file.note}`).join('\n')}
 `
   console.robolog(`Creating pull request`)
-  const {data} = await github.post(`/repos/${repoName}/pulls`, {
+  const {data} = await github.post(`/repos/${github.repoName}/pulls`, {
     title: `Add community documentation`,
-    head: branchName,
+    head: github.branchName,
     base: 'master',
     body: body
   })
